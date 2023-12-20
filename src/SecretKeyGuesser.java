ea649612831100233c555e8a16cad6562e62e00d
Big Kent <68211848+big-kent@users.noreply.github.com>
2023-12-20 12:04:23 +0700

    Kent adding method
    Hill Climbing is using to optimize the guesser method.

--- src/SecretKeyGuesser.java
+++ src/SecretKeyGuesser.java
@@ -54,46 +54,114 @@
 //    }
 //}
 
-//Second method:
+//Second method by Kiet: *********************
+// import java.util.Random;
+
+//     public class SecretKeyGuesser {
+//         public void start() {
+//             SecretKey key = new SecretKey();
+//             String str = "MMMMMMMMMMMM";
+//             while (key.guess(str) != 12) {
+//                 str = next(str);
+//                 System.out.println("Guessing... " + str);
+//             }
+//             System.out.println("I found the secret key. It is " + str);
+//         }
+
+
+//         private StringBuilder generateRandomKey() {
+//             StringBuilder randomKey = new StringBuilder();
+//             for (int i = 0; i < 12; i++) {
+//                 randomKey.append(POSSIBLE_CHARACTERS[random.nextInt(POSSIBLE_CHARACTERS.length)]);
+//             }
+//             return randomKey;
+//         }
+
+//         private StringBuilder improveGuess(StringBuilder currentGuess, int matchedPositions) {
+//             StringBuilder newGuess = new StringBuilder(currentGuess);
+
+//             for (int i = 0; i < 12; i++) {
+//                 if (newGuess.charAt(i) != secretKey.charAt(i)) {
+//                     double prob = random.nextDouble();
+//                     double matchProb = (double) matchedPositions / 12;
+
+//                     if (prob < matchProb) {
+//                         newGuess.setCharAt(i, secretKey.charAt(i));
+//                     } else {
+//                         newGuess.setCharAt(i, POSSIBLE_CHARACTERS[random.nextInt(POSSIBLE_CHARACTERS.length)]);
+//                     }
+//                 }
+//             }
+
+//             return newGuess;
+//         }
+//     }
+
+//Third method by Kent:**************************
+// Base on Brute-force approach algorithm
+// Optimize with Hill Climbling Algorithm
+
+
+// SecretKeyGuesser.java
+
 import java.util.Random;
 
-    public class SecretKeyGuesser {
-        public void start() {
-            SecretKey key = new SecretKey();
-            String str = "MMMMMMMMMMMM";
-            while (key.guess(str) != 12) {
-                str = next(str);
-                System.out.println("Guessing... " + str);
-            }
-            System.out.println("I found the secret key. It is " + str);
-        }
+public class SecretKeyGuesser {
+    private static final char[] OPTIONS = {'M', 'O', 'C', 'H', 'A'};
+    private static final int KEY_LENGTH = 12;
 
+    public void start() {
+        SecretKey secretKey = new SecretKey(generateRandomKey());
+        int counter = 0;
 
-        private StringBuilder generateRandomKey() {
-            StringBuilder randomKey = new StringBuilder();
-            for (int i = 0; i < 12; i++) {
-                randomKey.append(POSSIBLE_CHARACTERS[random.nextInt(POSSIBLE_CHARACTERS.length)]);
-            }
-            return randomKey;
-        }
+        while (true) {
+            counter++;
+            String currentGuess = generateRandomKey();
+            int currentMatchCount = secretKey.guess(currentGuess);
 
-        private StringBuilder improveGuess(StringBuilder currentGuess, int matchedPositions) {
-            StringBuilder newGuess = new StringBuilder(currentGuess);
-
-            for (int i = 0; i < 12; i++) {
-                if (newGuess.charAt(i) != secretKey.charAt(i)) {
-                    double prob = random.nextDouble();
-                    double matchProb = (double) matchedPositions / 12;
-
-                    if (prob < matchProb) {
-                        newGuess.setCharAt(i, secretKey.charAt(i));
-                    } else {
-                        newGuess.setCharAt(i, POSSIBLE_CHARACTERS[random.nextInt(POSSIBLE_CHARACTERS.length)]);
-                    }
-                }
+            if (currentMatchCount == KEY_LENGTH) {
+                System.out.println("Correct Secret Key: " + currentGuess);
+                System.out.println("Number of Guesses: " + counter);
+                break;
             }
 
-            return newGuess;
+            // Hill Climbing
+            String neighborGuess = generateNeighbor(currentGuess);
+            int neighborMatchCount = secretKey.guess(neighborGuess);
+
+            if (neighborMatchCount > currentMatchCount) {
+                // Move to the better neighbor
+                currentGuess = neighborGuess;
+            }
         }
     }
+
+    private String generateRandomKey() {
+        StringBuilder key = new StringBuilder();
+        for (int i = 0; i < KEY_LENGTH; i++) {
+            key.append(randomChar());
+        }
+        return key.toString();
+    }
+
+    private char randomChar() {
+        return OPTIONS[new Random().nextInt(OPTIONS.length)];
+    }
+
+    private String generateNeighbor(String currentGuess) {
+        // Generate a neighbor by randomly changing one character in the current guess
+        int indexToChange = new Random().nextInt(KEY_LENGTH);
+        char[] neighborKey = currentGuess.toCharArray();
+        neighborKey[indexToChange] = randomChar();
+        return new String(neighborKey);
+    }
+
+    public static void main(String[] args) {
+        SecretKeyGuesser guesser = new SecretKeyGuesser();
+        guesser.start();
+    }
+}
+
+
+
 
